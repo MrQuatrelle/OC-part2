b211d26a4e0c29ec027e2e9031c029b1dde77aca
MrQuatrelle <maigasmendo@gmail.com>
2023-10-10 21:44:00 +0100

    formatting SimpleProgram

--- src/SimpleProgram.c
+++ src/SimpleProgram.c
@@ -2,50 +2,50 @@
 
 int main() {
 
-  // set seed for random number generator
-  srand(0);
+    // set seed for random number generator
+    srand(0);
 
-  int clock1, value;
+    int clock1, value;
 
-  for(int n = 1; n <= DRAM_SIZE/4; n*=WORD_SIZE) {
+    for (int n = 1; n <= DRAM_SIZE / 4; n *= WORD_SIZE) {
 
-    reset_time();
-    init_cache();
+        reset_time();
+        init_cache();
 
-    printf("\nNumber of words: %d\n", (n-1)/WORD_SIZE + 1);
-    
-    for(int i = 0; i < n; i+=WORD_SIZE) {
-      write(i, (unsigned char *)(&i));
-      clock1 = get_time();
-      printf("Write; Address %d; Value %d; Time %d\n", i, i, clock1);
+        printf("\nNumber of words: %d\n", (n - 1) / WORD_SIZE + 1);
+
+        for (int i = 0; i < n; i += WORD_SIZE) {
+            write(i, (unsigned char*)(&i));
+            clock1 = get_time();
+            printf("Write; Address %d; Value %d; Time %d\n", i, i, clock1);
+        }
+
+        for (int i = 0; i < n; i += WORD_SIZE) {
+            read(i, (unsigned char*)(&value));
+            clock1 = get_time();
+            printf("Read; Address %d; Value %d; Time %d\n", i, value, clock1);
+        }
     }
 
-    for(int i = 0; i < n; i+=WORD_SIZE) {
-      read(i, (unsigned char *)(&value));
-      clock1 = get_time();
-      printf("Read; Address %d; Value %d; Time %d\n", i, value, clock1);
-    }  
+    printf("\nRandom accesses\n");
 
-  }
-
-  printf("\nRandom accesses\n");
-
-  // Do random accesses to the cache
-  for(int i = 0; i < 100; i++) {
-    int address = rand() % (DRAM_SIZE/4);
-    address = address - address % WORD_SIZE;
-    int mode = rand() % 2;
-    if (mode == MODE_READ) {
-      read(address, (unsigned char *)(&value));
-      clock1 = get_time();
-      printf("Read; Address %d; Value %d; Time %d\n", address, value, clock1);
+    // Do random accesses to the cache
+    for (int i = 0; i < 100; i++) {
+        int address = rand() % (DRAM_SIZE / 4);
+        address = address - address % WORD_SIZE;
+        int mode = rand() % 2;
+        if (mode == MODE_READ) {
+            read(address, (unsigned char*)(&value));
+            clock1 = get_time();
+            printf("Read; Address %d; Value %d; Time %d\n", address, value,
+                   clock1);
+        } else {
+            write(address, (unsigned char*)(&address));
+            clock1 = get_time();
+            printf("Write; Address %d; Value %d; Time %d\n", address, address,
+                   clock1);
+        }
     }
-    else {
-      write(address, (unsigned char *)(&address));
-      clock1 = get_time();
-      printf("Write; Address %d; Value %d; Time %d\n", address, address, clock1);
-    }
-  }
-  
-  return 0;
+
+    return 0;
 }
